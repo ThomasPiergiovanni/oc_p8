a516523a5dfe7176743c15004cd7c09e62fa5cd7
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-03-14 16:49:04 +0100

    all html intgegrated into tempaltes

--- pur_beurre_project/supersub/urls.py
+++ pur_beurre_project/supersub/urls.py
@@ -6,8 +6,10 @@ app_name = 'supersub'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('results/', views.results, name='results'),
     path('account/', views.account, name='account'),
     path('aliment/', views.aliment, name='aliment'),
     path('create_account/', views.create_account, name='create_account'),
+    path('login/', views.login, name='login'),
+    path('registered_aliments/', views.registered_aliments, name='registered_aliments'),
+    path('results/', views.results, name='results'),
 ]
