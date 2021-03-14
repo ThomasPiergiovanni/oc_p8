d6962f5382a889c5ba8513a6080fab5d4f846417
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-03-14 12:31:41 +0100

    base html created. starting using block content

--- pur_beurre_project/supersub/urls.py
+++ pur_beurre_project/supersub/urls.py
@@ -6,4 +6,5 @@ app_name = 'supersub'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('results/', views.results, name='results'),
 ]
--- pur_beurre_project/supersub/views.py
+++ pur_beurre_project/supersub/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'supersub/index.html')
+
+def results(request):
+    return render(request, 'supersub/results.html')
