57e903dfcdd35234bce6bab0770ba3b2ec4ce063
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-03-19 06:11:38 +0100

    creating category model

--- pur_beurre_project/supersub/models.py
+++ pur_beurre_project/supersub/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    id_origin = models.Charfield(max_lenght=200)
+    name = models.Charfield(max_lenght=200)
+    url = models.URLField(max_lenght=200)
