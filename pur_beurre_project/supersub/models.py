89e29bd9406b6372444da1efad5a92bbf587da61
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-03-20 19:19:15 +0100

    rename attribute product_name into name

--- pur_beurre_project/supersub/models.py
+++ pur_beurre_project/supersub/models.py
@@ -6,10 +6,13 @@ class Category(models.Model):
     name = models.CharField(max_length=200)
     url = models.URLField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     id_origin = models.CharField(max_length=200)
-    product_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
     nutriscore_grade = models.CharField(max_length=8)
     fatty_acids = models.DecimalField(max_digits=8, decimal_places=3)
     saturated_fatty_acids = models.DecimalField(max_digits=8, decimal_places=3)
@@ -17,3 +20,6 @@ class Product(models.Model):
     salt = models.DecimalField(max_digits=8, decimal_places=3)
     image = models.URLField(max_length=200)
     url = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.name
