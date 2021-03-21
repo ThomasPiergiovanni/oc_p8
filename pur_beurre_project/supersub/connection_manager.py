55097bf0b68e451db5d64987f740c54672121f0e
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-03-21 19:34:55 +0100

    adding connection manager file

---
+++ pur_beurre_project/supersub/connection_manager.py
@@ -0,0 +1,17 @@
+""" Connection manager module
+"""
+import request
+
+
+class ConnectionManager:
+    """
+    """
+    def download(self):
+        """
+        """
+        try:
+            pass
+        except:
+            pass
+
+
