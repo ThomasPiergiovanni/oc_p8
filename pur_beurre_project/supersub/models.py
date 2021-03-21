80bdf1c6e9d29e4be04d35c40bfd379244b6f826
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-03-21 11:52:47 +0100

    creating user favorite by adding many to many relation in user. not by creating favorite class itself

--- pur_beurre_project/supersub/models.py
+++ pur_beurre_project/supersub/models.py
@@ -34,3 +34,4 @@ class User(models.Model):
     first_name = models.CharField(max_length=200)
     email = models.EmailField(max_length=254)
     password = models.CharField(max_length=200)
+    favorites = models.ManyToManyField(ProductCategory)
