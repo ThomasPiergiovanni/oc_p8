474c8f9387bce57d95df4387decccd07fff902ae
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-03-21 12:52:43 +0100

    all modesl done. should be operationnal

--- pur_beurre_project/supersub/models.py
+++ pur_beurre_project/supersub/models.py
@@ -35,3 +35,6 @@ class User(models.Model):
     email = models.EmailField(max_length=254)
     password = models.CharField(max_length=200)
     favorites = models.ManyToManyField(ProductCategory)
+
+    def __str__(self):
+        return self.email
