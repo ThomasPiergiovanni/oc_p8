5a6de61e3b10f43d083ae875af1a1210fe8a5437
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-03-21 21:49:46 +0100

    adding connection_manager. response from off for catgory is ok

--- pur_beurre_project/supersub/connection_manager.py
+++ pur_beurre_project/supersub/connection_manager.py
@@ -1,17 +1,50 @@
 """ Connection manager module
 """
-import request
+import requests
 
 
 class ConnectionManager:
     """
     """
-    def download(self):
+    CATEGORIES_ENDPOINT = "https://fr.openfoodfacts.org/categories.json"
+    SELECTED_CATEGORIES = ["en:snacks", "en:desserts", "en:breads",
+                       "en:breakfast-cereals", "en:meals"]
+    PRODUCTS_AMOUNT = 50
+
+    def __init__(self):
+        self.endpoint = None
+        self.params = None
+
+    def download_categories(self):
         """
         """
         try:
+            endpoint = "https://fr.openfoodfacts.org/categories.json"
+            payload = {}
+            header = {}
+            response = requests.get(endpoint, headers=header, data=payload)
+            response_json = response.json()
+            print(response_json)
+        except:
+            pass
+            print("problem")
+
+    def download_products(self):
+        """
+        """
+        try:
+            self.endpoint = "https://fr.openfoodfacts.org/cgi/search.pl"
+            self.params = {
+            "action": "process", "tagtype_0": "categories",
+            "tag_contains_0": "contains", "tag_0": category.id_origin,
+            "json": 1, "page": 1, "page_size": PRODUCTS_AMOUNT}
             pass
         except:
             pass
+    
 
 
+
+if __name__ == "__main__":
+    connection_manager = ConnectionManager()
+    connection_manager.download_categories()
