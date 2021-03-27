9082b869f7ac785815ebe3db5e93290617a03730
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-03-27 12:55:25 +0100

    refactoring offapimanager class

--- pur_beurre_project/supersub/off_api_manager.py
+++ pur_beurre_project/supersub/off_api_manager.py
@@ -8,14 +8,10 @@ import requests
 class OffApiManager:
     """
     """
-
-    SELECTED_CATEGORIES = ["en:snacks", "en:desserts", "en:breads",
-                       "en:breakfast-cereals", "en:meals"]
-    PRODUCTS_AMOUNT = 1
-
     def __init__(self):
         self.endpoint = "https://fr.openfoodfacts.org/cgi/search.pl?"
-        self.params = None
+        self.header = {}
+        self.parameters = None
         self.categories = ["en:snacks", "en:desserts", "en:breads",
                        "en:breakfast-cereals", "en:meals"]
         self.products_amount = 1
@@ -25,13 +21,13 @@ class OffApiManager:
         """
         """
         try:
-            for category in self.categories: 
-                params = {
+            for category in self.categories:
+                self.parameters = {
                     "action": "process", "tagtype_0": "categories",
                     "tag_contains_0": "contains", "tag_0": category,
-                    "json": 1, "page": 1, "page_size": self.products_amount}
-                header = {}
-                response_api = requests.get(self.endpoint, headers=header, params=params)
+                    "json": 1, "page": 1, "page_size": self.products_amount} 
+                response_api = requests.get(
+                    self.endpoint, headers=self.header, params=self.parameters)
                 result = response_api.json()
                 print(result)
 
