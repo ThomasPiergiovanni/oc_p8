9fc6319f981a49ab923abc7f1587e6c076b400ce
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-03-27 18:56:52 +0100

    db manager class is progressing

--- pur_beurre_project/supersub/db_manager.py
+++ pur_beurre_project/supersub/db_manager.py
@@ -12,6 +12,7 @@ class DbManager():
         off_api_manager = OffApiManager()
         off_api_manager.download_products()
         self.source_data = off_api_manager.products
+        self.insert_products()
 
     def insert_products(self):
         for category in self.source_data:
@@ -19,35 +20,34 @@ class DbManager():
                 try:
                     if (
                             raw_product['id'] and
-                            raw_product['product_name']
-                            # raw_product['nutriscore_grade'] and
-                            # raw_product['fat_100g'] and
+                            raw_product['product_name'] and
+                            raw_product['nutriscore_grade'] and
+                            raw_product['nutriments']['fat_100g']
                             # raw_product['saturated-fat_100g'] and
                             # raw_product['sugars_100g'] and
                             # raw_product['salt_100g'] and
                             # raw_product['image_small_url'] and
                             # raw_product['url'] and
                             # raw_product['categories_tags']
-                        ):
-                        # print(raw_product['product_name'])
+                    ):
                         product = Product(
-                             id_origin=raw_product['id'],
-                             name=raw_product['product_name'])
-                        #     nutriscore_grade=raw_product['nutriscore_grade'],
-                        #     fatty_acids=raw_product['fat_100g'],
+                            id_origin=raw_product['id'],
+                            name=raw_product['product_name'],
+                            nutriscore_grade=raw_product['nutriscore_grade'],
+                            fatty_acids=raw_product['nutriments']['fat_100g'])
                         #     saturated_fatty_acids=raw_product['saturated-fat_100g'],
                         #     sugar=raw_product['sugars_100g'],
                         #     salt=raw_product['salt_100g'],
                         #     image=raw_product['image_small_url'],
                         #     url=raw_product['url'],
-                        #     categories=raw_product['categories_tags'])
-                        product.save()
+                        #     categories=raw_product['categories_tags']
+                        print(product.id_origin,
+                            product.name,
+                            product.nutriscore_grade,
+                            product.fatty_acids)
+                        #product.save()
                 except KeyError:
                     pass
     
     def get_products(self):
         pass
-
-# if __name__ == "__main__":
-#     db_manager = DbManager()
-#     db_manager.insert_products()
--- pur_beurre_project/supersub/off_api_manager.py
+++ pur_beurre_project/supersub/off_api_manager.py
@@ -4,7 +4,6 @@
 import requests
 
 
-
 class OffApiManager:
     """
     """
