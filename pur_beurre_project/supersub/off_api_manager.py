49b1845fdf726002911c274e6dd180d377880d67
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-03-27 13:09:02 +0100

    reaftoring Off api manager class

--- pur_beurre_project/supersub/off_api_manager.py
+++ pur_beurre_project/supersub/off_api_manager.py
@@ -15,31 +15,21 @@ class OffApiManager:
         self.categories = ["en:snacks", "en:desserts", "en:breads",
                        "en:breakfast-cereals", "en:meals"]
         self.products_amount = 1
-        self.cat_response = None
+        self.products = []
 
     def download_products(self):
         """
         """
-        try:
-            for category in self.categories:
-                self.parameters = {
-                    "action": "process", "tagtype_0": "categories",
-                    "tag_contains_0": "contains", "tag_0": category,
-                    "json": 1, "page": 1, "page_size": self.products_amount} 
-                response_api = requests.get(
-                    self.endpoint, headers=self.header, params=self.parameters)
-                result = response_api.json()
-                print(result)
-
-
-        except TypeError as error:
-            print(error)
-            pass
-    
-
+        for category in self.categories:
+            self.parameters = {
+                "action": "process", "tagtype_0": "categories",
+                "tag_contains_0": "contains", "tag_0": category,
+                "json": 1, "page": 1, "page_size": self.products_amount} 
+            response = requests.get(
+                self.endpoint, headers=self.header, params=self.parameters)
+            response = response.json()
+            self.products.append(response)
 
 if __name__ == "__main__":
     off_api_manager = OffApiManager()
-    # connection_manager.download_categories()
-    # connection_manager.filter_categories()
     off_api_manager.download_products()
