6c3d8a71695bfc68988807e7a63b10c9fddb1dab
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-03-27 14:26:03 +0100

    starting populate db method method from dbmanager

--- pur_beurre_project/supersub/db_manager.py
+++ pur_beurre_project/supersub/db_manager.py
@@ -10,3 +10,13 @@ class DbManager:
         off_api_manager = OffApiManager()
         off_api_manager.download_products()
         self.source_data = off_api_manager.products
+
+    def populate_db(self):
+        for category in self.source_data:
+            for product in category["products"]:
+                if product["product_name"]:
+                    print(product["product_name"])
+
+if __name__ == "__main__":
+    db_manager = DbManager()
+    db_manager.populate_db()
