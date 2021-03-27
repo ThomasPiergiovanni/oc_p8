5a3ae37d1a82080f9555ddd0ffae7527cda75419
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-03-27 12:49:20 +0100

    refactoring offapimanager class

--- pur_beurre_project/supersub/off_api_manager.py
+++ pur_beurre_project/supersub/off_api_manager.py
@@ -8,65 +8,30 @@ import requests
 class OffApiManager:
     """
     """
-    CATEGORIES_ENDPOINT = "https://fr.openfoodfacts.org/categories.json"
+
     SELECTED_CATEGORIES = ["en:snacks", "en:desserts", "en:breads",
                        "en:breakfast-cereals", "en:meals"]
-    PRODUCTS_AMOUNT = 50
+    PRODUCTS_AMOUNT = 1
 
     def __init__(self):
-        self.endpoint = None
+        self.endpoint = "https://fr.openfoodfacts.org/cgi/search.pl?"
         self.params = None
+        self.categories = ["en:snacks", "en:desserts", "en:breads",
+                       "en:breakfast-cereals", "en:meals"]
+        self.products_amount = 1
         self.cat_response = None
 
-    def download_categories(self):
+    def download_products(self):
         """
         """
         try:
-            endpoint = "https://fr.openfoodfacts.org/categories.json"
-            payload = {}
-            header = {}
-            response = requests.get(endpoint, headers=header, data=payload)
-            self.cat_response = response.json()
-            # print(self.cat_response)
-        except:
-            pass
-            print("problem")
-
-#{'known': 0, 'id': 'fr:vinaigre-au-combava', 'name': 'vinaigre-au-combava', 'url': 'https://fr.openfoodfacts.org/categorie/vinaigre-au-combava', 'products': 2}
-
-    def filter_categories(self, selected_categories = SELECTED_CATEGORIES):
-        
-        try:
-            for categories in self.cat_response["tags"]:
-                if categories["id"] in selected_categories and \
-                        categories["name"] and categories["url"]:
-                    valid_category = (
-                        categories["id"], categories["name"], categories["url"])
-                    print(valid_category)
-            # for category in categories:
-            #     print(category)
-                # if category["id"] in SELECTED_CATEGORIES and \
-                #         category["name"] and category["url"]:
-                #     valid_category = (
-                #         category["id"], category["name"], category["url"])
-                #     print(category["name"])
-        except KeyError:
-            pass
-
-    def download_products(
-            self, selected_categories = SELECTED_CATEGORIES,
-            product_amount = PRODUCTS_AMOUNT):
-        """
-        """
-        try:
-            for category in selected_categories:
-                endpoint = "https://fr.openfoodfacts.org/cgi/search.pl?"  
+            for category in self.categories: 
                 params = {
                     "action": "process", "tagtype_0": "categories",
                     "tag_contains_0": "contains", "tag_0": category,
-                    "json": 1, "page": 1, "page_size": product_amount}
+                    "json": 1, "page": 1, "page_size": self.products_amount}
                 header = {}
-                response_api = requests.get(endpoint, headers=header, params=params)
+                response_api = requests.get(self.endpoint, headers=header, params=params)
                 result = response_api.json()
                 print(result)
 
