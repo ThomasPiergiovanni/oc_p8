61976e17be5d2cc632a53697758e08448a0d8879
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-04-05 15:19:37 +0200

    migration of authentification to ensure email is unique in db

--- pur_beurre_project/authentification/models.py
+++ pur_beurre_project/authentification/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class User(models.Model):
     first_name = models.CharField(max_length=200)
-    email = models.EmailField(max_length=254)
+    email = models.EmailField(max_length=254, unique=True)
     password = models.CharField(max_length=200)
 
     def __str__(self):
