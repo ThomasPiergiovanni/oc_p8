48f0ed9d58a3316414dc261b60853960697a58ef
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-04-05 19:28:13 +0200

    usin httpresponse redirect after dtabase update

--- pur_beurre_project/authentification/views.py
+++ pur_beurre_project/authentification/views.py
@@ -15,42 +15,6 @@ def account(request):
     else:
         return login(request)
 
-# def create_account(request):
-#     context ={
-#         'message': "Connection",
-#         'button_message': "Créer"
-#     }
-#     return render(request, 'authentification/create_account.html', context)
-
-# def create_account(request):
-
-#     if request.method == 'POST':
-#         try:
-#             email = request.POST['create_account_email']
-#             existing_user = User.objects.get(email__iexact=email)
-#             context ={
-#                 'error_message': " est déja utilisé. Choissisez un autre nom.",
-#                 'email': existing_user.email
-#             }
-#             return render(request, 'authentification/create_account.html', context)
-#         except:
-#             email = request.POST['create_account_email']
-#             password = request.POST['create_account_password']
-#             first_name = request.POST['create_account_first_name']
-#             user = User(
-#                 first_name=first_name,
-#                 email=email,
-#                 password=password
-#             )
-#             print(user.first_name)
-#             user.save()
-#             context = {
-#                 'error_message': " n'est pas utilisé",
-#                 'email': email,
-#             }
-#             return render(request, 'authentification/create_account.html', context)
-#     else:
-#         return render(request, 'authentification/create_account.html') 
 
 def create_account(request):
 
@@ -80,8 +44,7 @@ def create_account(request):
                     'error_message': " n'est pas utilisé et a été créer",
                     'email': email,
                 }
-                return render(request, 'authentification/create_account.html', context)
-
+                return HttpResponseRedirect(reverse('supersub:index'))
     else:
         create_account_form = CreateAccountForm()
         context = {
@@ -90,6 +53,8 @@ def create_account(request):
         return render(request, 'authentification/create_account.html', context) 
 
 def login(request):
+    """
+    """
     context ={
         'message': "Se connecter",
         'button_message': "Se connecter"
