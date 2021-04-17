ebf8e82c94ce88944a0d1dd3f5c7d53c217d6444
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-04-17 14:24:49 +0200

    mvt favorites is functionnal

--- pur_beurre_project/supersub/urls.py
+++ pur_beurre_project/supersub/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('product_detail/<int:id_product>', views.product_detail, name='product_detail'),
     path('registered_products/', views.registered_products, name='registered_products'),
+    # path('results/', views.results, name='results'),
     path('results/', views.results, name='results'),
-    path('register_product/<int:id_product>,/<int:id_user>', views.register_product, name='register_product'),
+    path('register_product/<int:id_product>/<int:id_user>', views.register_product, name='register_product'),
 ]
--- pur_beurre_project/supersub/views.py
+++ pur_beurre_project/supersub/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 from .models import Product, Favorites
+from authentication.models import CustomUser
 
 
 
@@ -16,16 +17,8 @@ def index(request):
 def product_detail(request, id_product):
     product = Product.objects.get(pk=id_product)
     context = {
-        'name': product.name,
-        'nutriscore_grade': product.nutriscore_grade,
-        'fat': product.fat,
-        'saturated_fat': product.saturated_fat,
-        'sugar': product.sugar,
-        'salt': product.salt,
-        'image': product.image,
-        'url': product.url
+        'product': product
     }
-
     return render(request, 'supersub/product_detail.html', context)
 
 
@@ -36,37 +29,45 @@ def registered_products(request):
 def results(request):
     """
     """
-    try:
-        products = Product.objects.filter(name__contains=request.POST['product'])
-        product = products[0]
-        products_in_catgeories = (
-            Product.objects.filter(category_id=product.category_id)
-            .filter(nutriscore_grade__lte=product.nutriscore_grade)
-            .exclude(id__exact=product.id)[:6]
-        )
-        context = {
-            'name': product.name,
-            'image': product.image,
-            'products_in_categories': products_in_catgeories
-        }
-        return render(request, 'supersub/results.html', context)
-    except:
-        context = {
-            'error_message': "Ce produit n'a pas été reconnu ou n'existe pas dans la base de donnée. Faites une nouvelle recherche"
-        }
-        return render(request, 'supersub/index.html', context)
+    if request.method == 'GET':
+        try:
+            products = Product.objects.filter(name__contains=request.GET['product'])
+            product = products[0]
+            products_in_catgeories = (
+                Product.objects.filter(category_id=product.category_id)
+                .filter(nutriscore_grade__lte=product.nutriscore_grade)
+                .exclude(id__exact=product.id)[:6]
+            )
+            context = {
+                'or_prod': product.id,
+                'name': product.name,
+                'image': product.image,
+                'products_in_categories': products_in_catgeories
+            }
+            return render(request, 'supersub/results.html', context)
+        except:
+            context = {
+                'error_message': "Ce produit n'a pas été reconnu ou n'existe pas dans la base de donnée. Faites une nouvelle recherche"
+            }
+            return render(request, 'supersub/index.html', context)
+
 
 def register_product(request, id_product, id_user):
+    product = Product.objects.get(pk=id_product)
     try:
         favorite = Favorites.objects.get(product_id__exact=id_product, custom_user_id__exact=id_user)
         if favorite is not None:
             context = {
-                'message': "Ce produit fait féja partit de vos favoris",
+                'product': product,
+                'favorite_message': "Ce produit fait déja parti de vos favoris"
             }
-            return render(request, 'supersub/results.html', context)
+            return render(request, 'supersub/product_detail.html', context)
     except:
-        pass
-    #         favorite = Favorites(custom_user_id=id_user, product_id=id_product)
-    #         favorite.save()
-    #         return HttpResponseRedirect(reverse('supersub:index'))
+        favorite = Favorites(product_id=id_product, custom_user_id=id_user)
+        favorite.save()
+        context = {
+                    'product': product,
+                    'favorite_message': "Ce produit a été ajouté vos favoris"
+                }
+        return render(request, 'supersub/product_detail.html', context)
 
