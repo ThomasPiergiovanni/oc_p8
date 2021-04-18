ce41a50b407dcf5580c081663c3090dc720fb9bb
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-04-18 17:37:17 +0200

    test resulst is operationnal. Want ot get rid of the try except logic and nee to apply all this to the result view

--- pur_beurre_project/supersub/views.py
+++ pur_beurre_project/supersub/views.py
@@ -1,3 +1,4 @@
+from django.core import serializers
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponseRedirect
@@ -10,7 +11,11 @@ from authentication.models import CustomUser
 
 # Create your views here.
 def index(request):
-    return render(request, 'supersub/index.html')
+    try:
+        del request.session['potential_favorites']
+        return render(request, 'supersub/index.html')
+    except:
+        return render(request, 'supersub/index.html')
 
 def product_detail(request, id_product):
     product = Product.objects.get(pk=id_product)
@@ -60,50 +65,68 @@ def register_product(request, id_product, id_user):
                 }
         return render(request, 'supersub/product_detail.html', context)
 
-def results(request):
-    """
-    """
-    if request.method == 'GET':
-        if request.GET['product']:
-            try:
-                matching_products = Product.objects.filter(name__contains=request.GET['product'])
-                product = matching_products[0]
-                products_list = (
-                    Product.objects.filter(category_id=product.category_id)
-                    .filter(nutriscore_grade__lte=product.nutriscore_grade)
-                    .exclude(id__exact=product.id).order_by('id')
-                )
-                paginator = Paginator(products_list, 6)
-
-                page_number = request.GET.get ('page')
-                page_obj = paginator.get_page(page_number)
-                context = {
-                    'searched_product': product,
-                    'products': products_list,
-                    'page_obj' : page_obj
-
-                }
-                return render(request, 'supersub/results_test.html', context)
-            except:
-                context = {
-                    'message': "Ce produit n'a pas été reconnu ou n'existe pas dans la base de donnée. Faites une nouvelle recherche"
-                }
-                return render(request, 'supersub/index.html', context)
-        else:
-            pass
-
-
 def results_test(request):
-    products_list = (
-        Product.objects.all().order_by('id')
-    )
-    paginator = Paginator(products_list, 6)
+    """
+    """
+    PRODUCTS_LIST = []
+    try: 
+        if request.session['potential_favorites']:
+            matching_products =[]
+            for deserialized_object in serializers.deserialize("json", request.session['potential_favorites']):
+                product = Product.objects.get(pk=deserialized_object.object.pk)
+                matching_products.append(product)
 
-    page_number = request.GET.get ('page')
-    page_obj = paginator.get_page(page_number)
-    context = {
-        'page_obj' : page_obj
+            paginator = Paginator(matching_products, 6)
 
-    }
-    print(request.META.get('REQUEST_METHOD'))
-    return render(request, 'supersub/results_test.html', context)
+            page_number = request.GET.get ('page')
+            page_obj = paginator.get_page(page_number)
+            context = {
+                # 'searched_product': product,
+                # 'products': products_list,
+                'page_obj' : page_obj
+
+            }
+            return render(request, 'supersub/results_test.html', context)
+    except:
+        try:
+            name_matching = Product.objects.filter(name__contains=request.GET['product'])
+            product = name_matching[0]
+            matching_products = (
+                Product.objects.filter(category_id=product.category_id)
+                .filter(nutriscore_grade__lte=product.nutriscore_grade)
+                .exclude(id__exact=product.id).order_by('id')
+            )
+            request.session['potential_favorites'] = serializers.serialize('json',matching_products)
+
+            paginator = Paginator(matching_products, 6)
+
+            page_number = request.GET.get ('page')
+            page_obj = paginator.get_page(page_number)
+            context = {
+                # 'searched_product': product,
+                # 'products': products_list,
+                'page_obj' : page_obj
+
+            }
+            return render(request, 'supersub/results_test.html', context)
+   
+        except:
+            context = {
+                'message': "Ce produit n'a pas été reconnu ou n'existe pas dans la base de donnée. Faites une nouvelle recherche"
+            }
+            return render(request, 'supersub/index.html', context)
+
+# def results_test(request):
+#     products_list = (
+#         Product.objects.all().order_by('id')
+#     )
+#     paginator = Paginator(products_list, 6)
+
+#     page_number = request.GET.get ('page')
+#     page_obj = paginator.get_page(page_number)
+#     context = {
+#         'page_obj' : page_obj
+
+#     }
+#     print(request.META.get('REQUEST_METHOD'))
+#     return render(request, 'supersub/results_test.html', context)
