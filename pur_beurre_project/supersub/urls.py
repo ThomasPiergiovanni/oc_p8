29649ff2f4485b68d69494b23a78682ab247a017
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-05-02 20:38:43 +0200

    changed register favorite url. userd id is not more a parameters? user id is used in view through request.user.id

--- pur_beurre_project/supersub/urls.py
+++ pur_beurre_project/supersub/urls.py
@@ -8,20 +8,9 @@ app_name = 'supersub'
 urlpatterns = [
     
     path('', IndexView.as_view(), name='index'),
-    # path('', views.index, name='index'),
-    
     path('product_detail/<int:id_product>', ProductDetailView.as_view(), name='product_detail'),
-    # path('product_detail/<int:id_product>', views.product_detail, name='product_detail'),
-
     path('favorites/', FavoritesView.as_view(), name='favorites'),
-    # path('registered_products/', views.registered_products, name='registered_products'),
-
     path('results/', ResutlView.as_view(), name='results'),
-    # path('results/', views.results, name='results'),
-
-
-    path('register_favorite/<int:id_product>/<int:id_user>', RegisterFavoriteView.as_view(), name='register_favorite' ),
-    # path('register_product/<int:id_product>/<int:id_user>', views.register_product, name='register_product'),
-
+    path('register_favorite/<int:id_product>', RegisterFavoriteView.as_view(), name='register_favorite' ),
     path('legal_mentions/', LegalMentionsView.as_view(), name='legal_mentions')
 ]
--- pur_beurre_project/supersub/views.py
+++ pur_beurre_project/supersub/views.py
@@ -141,15 +141,21 @@ class FavoritesView(View):
 class RegisterFavoriteView(View):
     """
     """
-    def get(self, request, id_product, id_user):
+    def get(self, request, id_product):
         """
         """
-        if SupersubManager()._get_favorite(id_product, id_user):
-            messages.add_message(request, messages.WARNING,"Produit déja enregistré")
+        if SupersubManager()._get_favorite(id_product, request.user.id):
+            messages.add_message(
+                request, messages.WARNING,"Produit déja enregistré")
         else:
-            Favorites(product_id=id_product, custom_user_id=id_user).save()
-            messages.add_message(request, messages.SUCCESS,"Produit enregistré!")
-        return HttpResponseRedirect(reverse('supersub:product_detail', args=[id_product]))
+            Favorites(
+                product_id=id_product,
+                custom_user_id=request.user.id).save()
+            messages.add_message(
+                request,
+                messages.SUCCESS,"Produit enregistré!")
+        return HttpResponseRedirect(
+            reverse('supersub:product_detail', args=[id_product]))
 
 
 class LegalMentionsView(View):
@@ -161,7 +167,7 @@ class LegalMentionsView(View):
         self.data = SupersubManager()._get_data()
         self.data['context']['navbar_form'] = NavbarSearchForm()
         self.data['render'] = 'supersub/legal_mentions.html'
-        
+
     def get(self,request):
         """
         """
