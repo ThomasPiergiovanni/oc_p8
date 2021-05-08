f12fc38c54be463d5ec4ed10853728f746118c4c
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-05-08 19:52:25 +0200

    Tryin a bit more on functionnal test

--- pur_beurre_project/supersub/tests/functionnal/test_edge.py
+++ pur_beurre_project/supersub/tests/functionnal/test_edge.py
@@ -6,12 +6,13 @@ class NewVisitorTest(TestCase):
     """
     """
     def setUp(self):
-        self.browser = webdriver.Edge('C:\Program Files\EdgeDriver\msedgedriver.exe')
+        # self.browser = webdriver.Edge('C:\Program Files\EdgeDriver\msedgedriver.exe')
+        self.browser = webdriver.Edge()
     
     def tearDown(self):
         self.browser.quit()
     
-    def test_index_title_is_nok(self):
+    def test_index_has_correct_title(self):
         self.browser.get('http://127.0.0.1:8000/supersub')
         self.assertIn("P8 - Pur-beurre", self.browser.title)
     
