9781f3186ea1968f74b9a60bc3a5c8b8bb9f4b57
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-05-11 20:17:13 +0200

    trying to manage post request test. unsuccessfull so far

--- pur_beurre_project/supersub/tests/functionnal/test_edge.py
+++ pur_beurre_project/supersub/tests/functionnal/test_edge.py
@@ -7,7 +7,7 @@ class NewVisitorTest(TestCase):
     """
     def setUp(self):
         self.browser = webdriver.Edge('C:\Program Files\EdgeDriver\msedgedriver.exe')
-        self.browser = webdriver.Edge()
+        # self.browser = webdriver.Edge()
     
     def tearDown(self):
         self.browser.quit()
@@ -15,4 +15,8 @@ class NewVisitorTest(TestCase):
     def test_index_has_correct_title(self):
         self.browser.get('http://127.0.0.1:8000/supersub')
         self.assertIn("P8 - Pur-beurre", self.browser.title)
+
+    def test_index_has_main_form(self):
+        self.browser.get('http://127.0.0.1:8000/supersub/')
+        self.browser.find_element_by_id('product')
     
--- pur_beurre_project/supersub/tests/unit/test_views.py
+++ pur_beurre_project/supersub/tests/unit/test_views.py
@@ -1,10 +1,23 @@
 from django.template.loader import render_to_string
 from django.test import TestCase
+from django.urls import reverse
 
 
 class IndexViewTest(TestCase):
     """
     """
+    def test_index_GET_request(self):
+        response = self.client.get('/supersub/')
+        self.assertEqual(response.status_code, 200)
+
     def test_index_template_is_true(self):
         response = self.client.get('/supersub/')
         self.assertTemplateUsed(response, 'supersub/index.html')
+
+
+class ResultViewTest(TestCase):
+    """
+    """
+    def test_index_form_POST_request(self):
+        response = self.client.post('/supersub/',data={'product': "Nutella"})
+        self.assertIn("Nutella", response.content.decode())
