2a49a3de46514673ea75725c27e4cabb992bbddf
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-05-13 14:02:01 +0200

    refactoring using class method

--- pur_beurre_project/supersub/tests/unit/test_views.py
+++ pur_beurre_project/supersub/tests/unit/test_views.py
@@ -34,11 +34,20 @@ class ResultViewTest(TestCase):
     """
     @classmethod
     def setUpTestData(cls):
+        cls.emulate_category()
+        cls.emulate_product()
+    
+    @classmethod
+    def emulate_category(cls):
         Category.objects.create(
             id=1,
             name="CategorieOne",
             url="www.categorie_test.com")
+    
+    @classmethod
+    def emulate_product(cls):
         Product.objects.create(
+            id=1,
             id_origin = 'fevfvf',
             name = 'Product_for_test',
             nutriscore_grade = 'A',
@@ -50,11 +59,15 @@ class ResultViewTest(TestCase):
             url='www.urlbidon.com',
             categories='cat1, cat2, cat3',
             category_id=1
-        )
+        ) 
 
-    def test_index_page_POST_request(self):
-        pass
+    def test_index_page_POST_request_gets_response_200(self):
         response = self.client.post('/supersub/results/',data={'product':'Product_for_test'})
         self.assertEqual(response.status_code,200)
+    
+    def test_index_page_POST_request_gets_context_correct(self):
+        response = self.client.post('/supersub/results/',data={'product':'Product_for_test'})
+        product = Product.objects.get(pk=1)
+        self.assertEqual(response.context['searched_prod'], product)
 
 
