84cd5d5e14d39a91428e935e679b4aa2bb2a799f
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-05-22 22:04:03 +0200

    making progress on test register favorites view

--- pur_beurre_project/supersub/tests/unit/views/test_legal_mentions_view.py
+++ pur_beurre_project/supersub/tests/unit/views/test_legal_mentions_view.py
@@ -6,14 +6,16 @@ from supersub.forms import NavbarSearchForm
 class LegalMentionsViewTest(TestCase):
     """
     """
+    def setUp(self):
+        self.response = self.client.get('/supersub/legal_mentions/')
+
     def test_get_with_status_code_200(self):
-        response = self.client.get('/supersub/legal_mentions/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
     def test_get_with_template(self):
-        response = self.client.get('/supersub/legal_mentions/')
-        self.assertTemplateUsed(response, 'supersub/legal_mentions.html')
+        self.assertTemplateUsed(self.response, 'supersub/legal_mentions.html')
     
     def test_get_with_navbar_form(self):
-        response = self.client.get('/supersub/legal_mentions/')
-        self.assertIsInstance(response.context['navbar_form'], NavbarSearchForm)
+        self.assertIsInstance(
+            self.response.context['navbar_form'],
+            NavbarSearchForm)
---
+++ pur_beurre_project/supersub/tests/unit/views/test_register_favorite_view.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+
+from authentication.models import CustomUser
+from authentication.tests.unit.models.test_custom_user import CustomUserTest
+from supersub.tests.unit.models.test_category import CategoryTest
+from supersub.tests.unit.models.test_product import ProductTest
+from supersub.tests.unit.models.test_favorites import FavoritesTest
+
+
+class RegisterFavoriteViewTest(TestCase):
+    """
+    """
+    @classmethod
+    def setUpTestData(cls):
+        CategoryTest().emulate_category()
+        ProductTest().emulate_product()
+        CustomUserTest().emulate_custom_user()
+        FavoritesTest().emulate_favorites()
+    
+    def setUp(self):
+        self.client.login(email='testuser@email.com', password='_Xxxxxxx')
+        
+    def test_get_with_status_code_200(self):
+        self.response = self.client.get('/supersub/register_favorite/1', follow=True)
+        self.assertEqual(self.response.status_code, 200)
+    
+    def test_get_with_message_warning(self):
+        self.response = self.client.get('/supersub/register_favorite/1', follow=True)
+        messages = self.response.context['messages']
+        for message in messages:
+            self.assertEqual(message.level_tag, 'warning')
+            self.assertEqual(message.message, "Produit déja enregistré")
+    
+    def test_get_with_message_success(self):
+        self.response = self.client.get('/supersub/register_favorite/2', follow=True)
+        messages = self.response.context['messages']
+        for message in messages:
+            self.assertEqual(message.level_tag, 'success')
+            self.assertEqual(message.message, "Produit enregistré!")
+
+
