83c548e9cf66180bb727121f7dd1015b2cdf68bf
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-05-22 20:59:42 +0200

    test product detail view completed

---
+++ pur_beurre_project/supersub/tests/unit/views/test_product_detail_view.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+
+from supersub.forms import NavbarSearchForm
+from supersub.models.product import Product
+from supersub.tests.unit.models.test_category import CategoryTest
+from supersub.tests.unit.models.test_product import ProductTest
+
+
+class ProductDetailViewTest(TestCase):
+    """
+    """
+    @classmethod
+    def setUpTestData(cls):
+        CategoryTest().emulate_category()
+        ProductTest().emulate_product()
+    
+    def setUp(self):
+        self.response = self.client.get('/supersub/product_detail/1')
+
+    def test_get_with_status_code_200(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_get_with_template(self):
+        self.assertTemplateUsed(self.response, 'supersub/product_detail.html')
+    
+    def test_get_with_navbar_form(self):
+        self.assertIsInstance(
+            self.response.context['navbar_form'],
+            NavbarSearchForm)
+    
+    def test_get_with_product(self):
+        product = Product.objects.get(pk=1)
+        self.assertEquals(self.response.context['prod'], product)
