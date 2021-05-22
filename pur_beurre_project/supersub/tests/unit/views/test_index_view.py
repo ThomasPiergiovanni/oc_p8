0d09fb2b37f04ddc4bb8308d8ab6ea59232dd2aa
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-05-22 18:13:39 +0200

    small refactoring

--- pur_beurre_project/supersub/tests/unit/views/test_index_view.py
+++ pur_beurre_project/supersub/tests/unit/views/test_index_view.py
@@ -6,18 +6,18 @@ from supersub.forms import MainSearchForm, NavbarSearchForm
 class IndexViewTest(TestCase):
     """
     """
-    def test_index_page_GET_request(self):
+    def test_get_with_status_code_200(self):
         response = self.client.get('/supersub/')
         self.assertEqual(response.status_code, 200)
 
-    def test_index_page_template_is_true(self):
+    def test_get_with_template(self):
         response = self.client.get('/supersub/')
         self.assertTemplateUsed(response, 'supersub/index.html')
     
-    def test_index_page_uses_main_form(self):
+    def test_index_with_main_form(self):
         response = self.client.get('/supersub/')
         self.assertIsInstance(response.context['main_form'], MainSearchForm)
     
-    def test_index_page_uses_navbar_form(self):
+    def test_index_with_navbar_form(self):
         response = self.client.get('/supersub/')
         self.assertIsInstance(response.context['navbar_form'], NavbarSearchForm)
