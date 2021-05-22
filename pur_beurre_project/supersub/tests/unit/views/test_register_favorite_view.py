4ddcaa24ef01f0f1a7df0e630274f8059045711e
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-05-22 22:47:51 +0200

    test register favorite in progress

--- pur_beurre_project/supersub/tests/unit/views/test_register_favorite_view.py
+++ pur_beurre_project/supersub/tests/unit/views/test_register_favorite_view.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from authentication.models import CustomUser
 from authentication.tests.unit.models.test_custom_user import CustomUserTest
+from supersub.models.favorites import Favorites
 from supersub.tests.unit.models.test_category import CategoryTest
 from supersub.tests.unit.models.test_product import ProductTest
 from supersub.tests.unit.models.test_favorites import FavoritesTest
@@ -31,11 +34,20 @@ class RegisterFavoriteViewTest(TestCase):
             self.assertEqual(message.level_tag, 'warning')
             self.assertEqual(message.message, "Produit déja enregistré")
     
+    def test_get_with_favorites_saved(self):
+        self.response = self.client.get('/supersub/register_favorite/2', follow=True)
+        new_favorite = Favorites.objects.get(
+            product_id__exact=2,
+            custom_user_id__exact=1)
+        self.assertTrue(new_favorite)
+
     def test_get_with_message_success(self):
         self.response = self.client.get('/supersub/register_favorite/2', follow=True)
         messages = self.response.context['messages']
         for message in messages:
             self.assertEqual(message.level_tag, 'success')
             self.assertEqual(message.message, "Produit enregistré!")
+    
+
 
 
