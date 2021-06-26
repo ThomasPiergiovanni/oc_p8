a4df2597f0a1d414bbaeb54bc9c91b8a8666763b
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-06-26 18:20:12 +0200

    adding functionnal tests to NewSearch Test

--- pur_beurre_project/supersub/tests/functionnal/test_edge.py
+++ pur_beurre_project/supersub/tests/functionnal/test_edge.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 
 from django.test import TestCase
 
-class NewVisitorTest(TestCase):
+class NewSearchTest(TestCase):
     """
     """
     def setUp(self):
@@ -12,9 +12,14 @@ class NewVisitorTest(TestCase):
     def tearDown(self):
         self.browser.quit()
     
-    def test_index_has_correct_title(self):
+    def test_index_head_has_correct_title(self):
         self.browser.get('http://127.0.0.1:8000/supersub')
         self.assertIn("P8 - Pur-beurre", self.browser.title)
+    
+    def test_index_header_has_correct_h1(self):
+        self.browser.get('http://127.0.0.1:8000/supersub')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('DU GRAS, OUI MAIS DE QUALITÉ', header_text)
 
     def test_index_has_main_form(self):
         self.browser.get('http://127.0.0.1:8000/supersub/')
