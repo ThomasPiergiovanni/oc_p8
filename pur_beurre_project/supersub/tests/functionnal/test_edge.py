1b0d205b10035177ce1ec45ddcf8dd60e6bbf303
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-06-26 19:49:09 +0200

    cosmetic changes on functionnal test

--- pur_beurre_project/supersub/tests/functionnal/test_edge.py
+++ pur_beurre_project/supersub/tests/functionnal/test_edge.py
@@ -22,38 +22,25 @@ class NewSearchTest(TestCase):
         header_h1_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('DU GRAS, OUI MAIS DE QUALITÉ', header_h1_text)
 
-        # The user notices a navbar composed of an image,
-        # a user icon, a food prod icon and a logout session icon
+        # The user notices a navbar composed of an image, a title,
+        # a form, a user icon, a food prod icon and a logout session icon
         navbar_image = self.browser.find_element_by_tag_name('img')
         self.assertEqual(
             navbar_image.get_attribute('src'),
             'http://127.0.0.1:8000/static/supersub/assets/img/'\
             'logo_pur_beurre.png'
         )
-
-        #... a title
         navbar_text = self.browser.find_element_by_id(
             'navbar_pur_beurre_texte'
         ).text
         self.assertIn("Pur Beurre", navbar_text)
-
-        # ...a form
         navbar_inputbox = self.browser.find_element_by_id(
             'navbar_search_form'
         )
         self.assertEqual(navbar_inputbox.get_attribute('method'),'post')
-
-        # ...an icon representing a user
-        navbar_user= self.browser.find_element_by_id('user_icon')
-        self.assertTrue(navbar_user)
-        
-        # ...an icon representing a carrot (its for the user favorites)
-        navbar_carrot= self.browser.find_element_by_id('carrot_icon')
-        self.assertTrue(navbar_carrot)
-
-        # ...and an icon representing logout
-        navbar_logout= self.browser.find_element_by_id('logout_icon')
-        self.assertTrue(navbar_logout)
+        self.assertTrue(self.browser.find_element_by_id('user_icon'))
+        self.assertTrue(self.browser.find_element_by_id('carrot_icon'))
+        self.assertTrue(self.browser.find_element_by_id('logout_icon'))
 
         # The user notices a form textbox as well as a search button
         inputbox = self.browser.find_element_by_id('index_main_search_form')
