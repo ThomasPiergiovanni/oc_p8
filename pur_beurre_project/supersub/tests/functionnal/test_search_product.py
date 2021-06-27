ea7ef6dd2ec768f5d1ae6415efdc2a7886e23bfd
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-06-27 17:39:13 +0200

    making progress on functionnal test

--- pur_beurre_project/supersub/tests/functionnal/test_search_product.py
+++ pur_beurre_project/supersub/tests/functionnal/test_search_product.py
@@ -1,9 +1,7 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
-
 from time import sleep
 
-from django.test import LiveServerTestCase
 
 class SearchProductTest(StaticLiveServerTestCase):
     """
@@ -21,19 +19,50 @@ class SearchProductTest(StaticLiveServerTestCase):
         cls.browser.quit()
         super().tearDownClass()
     
-
-    def test_search_product_use_case(self):
+    def setUp(self):
         # The user logs to the app page
         self.browser.get('%s%s' % (self.live_server_url, '/supersub/'))
-        # self.assertIn("P8 - Pur-beurre", self.browser.title)
-
-        sleep(5)
     
-        # When the user hits enter a new page is open with the products
-        # and ther subs prodcuts
-        self.browser.find_element_by_id('id_main_form').send_keys('Pain')
-        sleep(2)
+    def test_user_lands_on_home_page(self):
+        # The user notices the page name and the header title
+        self.assertIn("P8 - Pur-beurre", self.browser.title)
+        self.assertIn(
+            'DU GRAS, OUI MAIS DE QUALITÉ',
+            self.browser.find_element_by_tag_name('h1').text
+        )
 
+        # The user notices a navbar composed of an image 
+        navbar_image = self.browser.find_element_by_tag_name('img')
+        self.assertEqual(
+            navbar_image.get_attribute('src'),
+            '*/static/supersub/assets/img/'\
+            'logo_pur_beurre.png'
+        )
+
+        #...  a title
+        navbar_text = self.browser.find_element_by_id(
+            'navbar_pur_beurre_texte'
+        ).text
+        self.assertIn("Pur Beurre",navbar_text)
+
+        #... a form
+        navbar_inputbox = self.browser.find_element_by_id(
+            'navbar_search_form'
+        )
+        self.assertEqual(navbar_inputbox.get_attribute('method'),'post')
+
+        # ... a user, a carrot and a logout icons
+        self.assertTrue(self.browser.find_element_by_id('user_icon'))
+        self.assertTrue(self.browser.find_element_by_id('carrot_icon'))
+        self.assertTrue(self.browser.find_element_by_id('logout_icon'))
+
+    def test_search_product_use_case(self):
+        # The user types "Pain" on teh main form
+        sleep(2)
+        self.browser.find_element_by_id('id_main_form').send_keys('Pain')
+        sleep(5)
+
+        # The user clicks then "Chercher" button"
         self.browser.find_element_by_id('index_search_button').click()
         sleep(2)
         # self.assertTrue(
