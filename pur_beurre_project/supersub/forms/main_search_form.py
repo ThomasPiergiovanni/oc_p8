1932573d4c4e8aa8bb5147d6f2bba0b0913c9e10
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-06-27 23:20:05 +0200

    pep8 on supersub/forms repo

--- pur_beurre_project/supersub/forms/main_search_form.py
+++ pur_beurre_project/supersub/forms/main_search_form.py
@@ -1,12 +1,18 @@
+"""Main search form module
+"""
 from django import forms
 
 
 class MainSearchForm(forms.Form):
-    """
+    """Main search form class. Used for searching product substitute
     """
     product = forms.CharField(
         label=None,
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'autofocus': True,
-            'id':'id_main_form'}))
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'autofocus': True,
+                'id':'id_main_form'
+            }
+        )
+    )
--- pur_beurre_project/supersub/forms/navbar_search_form.py
+++ pur_beurre_project/supersub/forms/navbar_search_form.py
@@ -1,10 +1,17 @@
+"""Navigation bar search form module
+"""
 from django import forms
 
 
 class NavbarSearchForm(forms.Form):
-    """
+    """Navigation bar search form class. Used for searching product substitute
+    with the navbar form
     """
     product = forms.CharField(
         label=None,
-        widget=forms.TextInput(attrs={
-            'class': 'form-control mybox'}))
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control mybox'
+            }
+        )
+    )
