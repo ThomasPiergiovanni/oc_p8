d98dac9508652ff88a23608e5d142fba7573cc53
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-06-28 00:16:15 +0200

    working on test sign up functionnal test

--- pur_beurre_project/authentication/forms/sign_up_form.py
+++ pur_beurre_project/authentication/forms/sign_up_form.py
@@ -8,10 +8,43 @@ from authentication.models import CustomUser
 class SignUpForm(UserCreationForm):
     """
     """
-    first_name = forms.CharField(label="Prénom", widget=forms.TextInput(attrs={'class': 'form-control', 'autofocus': True, 'id':'id_first_name_form'}))
-    email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    password1 = forms.CharField(label="Mot de passe", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(label="Confirmer le mot de passe",widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(
+        label="Prénom",
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'autofocus': True,
+                'id':'id_first_name_input'
+            }
+        )
+    )
+    email = forms.CharField(
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'id':'id_email_input'
+            }
+        )
+    )
+    password1 = forms.CharField(
+        label="Mot de passe",
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'id':'id_pwd1_input'
+            }
+        )
+    )
+    password2 = forms.CharField(
+        label="Confirmer le mot de passe",
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'id':'id_pwd2_input'
+            }
+        )
+    )
+
     class Meta(UserCreationForm):
         model = CustomUser
         fields = ['first_name', 'email', 'password1', 'password2']
--- pur_beurre_project/authentication/tests/functional/test_sign_up.py
+++ pur_beurre_project/authentication/tests/functional/test_sign_up.py
@@ -33,7 +33,26 @@ class SignUpTest(StaticLiveServerTestCase):
         ))
     
     def test_sign_up_use_case(self):
-        # The user types its email on the main form
+        # The user types its first name, email and its password twice in 
+        # the form
         sleep(2)
-        self.browser.find_element_by_id('id_first_name_form').send_keys('tester')
-        sleep(2)
+        self.browser.find_element_by_id('id_first_name_input')\
+            .send_keys('Utilisateur')
+        sleep(1)
+        self.browser.find_element_by_id('id_email_input')\
+            .send_keys('utilisateur@email.com')
+        sleep(1)
+        self.browser.find_element_by_id('id_pwd1_input')\
+            .send_keys('_Yyyyyyy')
+        sleep(1)
+        self.browser.find_element_by_id('id_pwd2_input')\
+            .send_keys('_Yyyyyyy')
+        sleep(1)
+
+        # The user clicks then "Envoyer" button and lands on the sign in page
+        self.browser.find_element_by_id('id_sign_up_button').click()
+        sleep(5)
+        self.assertIn(
+            'CONNECTION',
+            self.browser.find_element_by_tag_name('h2').text
+        )
