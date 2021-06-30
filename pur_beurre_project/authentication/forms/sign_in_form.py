ba2ed0758e99c20a0a40f665485f735cc912e438
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-06-30 22:09:37 +0200

    test sign in use case is done

--- pur_beurre_project/authentication/forms/sign_in_form.py
+++ pur_beurre_project/authentication/forms/sign_in_form.py
@@ -6,5 +6,22 @@ from django.utils.translation import gettext_lazy as _
 class SignInForm(AuthenticationForm):
     """
     """
-    username = forms.CharField(label="Email", widget=forms.TextInput(attrs={'class': 'form-control', 'autofocus': True, 'id':'id_signin_email_input'}))
-    password = forms.CharField(label="Mot de passe",widget=forms.PasswordInput(attrs={'class': 'form-control', 'id':'id_signin_password_input'}))
+    username = forms.CharField(
+        label="Email",
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'autofocus': True,
+                'id':'id_signin_email_input'
+            }
+        )
+    )
+    password = forms.CharField(
+        label="Mot de passe",
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'id':'id_signin_password_input'
+            }
+        )
+    )
---
+++ pur_beurre_project/authentication/tests/functional/test_sign_in_use_case.py
@@ -0,0 +1,54 @@
+"""Module for sign in use case functional test
+"""
+from time import sleep
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+
+from authentication.tests.unit.models.test_custom_user import CustomUserTest
+
+
+class SignInUseCaseTest(StaticLiveServerTestCase):
+    """Sign in use case test class
+    """
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.browser = webdriver.Edge(
+            'C:\Program Files\EdgeDriver\msedgedriver.exe'
+        )
+        cls.browser.implicitly_wait(10)
+        CustomUserTest.emulate_custom_user()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.quit()
+        super().tearDownClass()
+    
+    def setUp(self):
+        # The user logs to the sign up page
+        self.browser.get(
+            '%s%s' % (
+                self.live_server_url,
+                '/authentication/sign_in/'
+            )
+        )
+
+    
+    def test_sign_in_use_case(self):
+        # The user types its email and password
+        sleep(2)
+        self.browser.find_element_by_id('id_signin_email_input')\
+            .send_keys('testuser@email.com')
+        sleep(1)
+        self.browser.find_element_by_id('id_signin_password_input')\
+            .send_keys('_Xxxxxxx')
+        sleep(1)
+
+        # The user clicks then "Envoyer" button and lands on the sign in page
+        self.browser.find_element_by_id('id_sign_in_button').click()
+        self.assertIn(
+            'DU GRAS, OUI MAIS DE QUALITÉ',
+            self.browser.find_element_by_tag_name('h1').text
+        )
+        sleep(2)
