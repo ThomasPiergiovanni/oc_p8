636877b947104f9620310780c36cfb934210969d
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-07-03 22:54:27 +0200

    test authentication manager unit test completed

--- pur_beurre_project/authentication/tests/unit/managers/test_authentication_manager.py
+++ pur_beurre_project/authentication/tests/unit/managers/test_authentication_manager.py
@@ -1,7 +1,7 @@
-from django.test import TestCase
 from django.contrib.auth import authenticate
+from django.contrib.sessions.middleware import SessionMiddleware
+from django.test import RequestFactory, TestCase
 
-from authentication.models import CustomUser 
 from authentication.managers.authentication_manager import AuthenticationManager
 from authentication.tests.unit.models.test_custom_user import CustomUserTest
 
@@ -12,28 +12,29 @@ class AuthenticationManagerTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         CustomUserTest.emulate_custom_user()
+        cls.auth_manager = AuthenticationManager()
     
     def setUp(self):
+        pass
+
+    def test_logout_with_client_logged_in(self):
         self.client.login(
             email='testuser@email.com',
             password='_Xxxxxxx'
         )
-
-    def test_logout_with_client_logged_in(self):
         self.assertFalse(
-            AuthenticationManager()._logout(self.client)
+            self.auth_manager._logout(self.client)
         )
     
     def test_authenticate_with_email_and_password(self):
         self.assertTrue(
-            AuthenticationManager()._authenticate('testuser@email.com','_Xxxxxxx')
+            self.auth_manager._authenticate('testuser@email.com','_Xxxxxxx')
         )
 
     def test_login_with_client_login(self):
-        user = authenticate(
-            email='testuser@email.com',
-            password='_Xxxxxxx'
-        )
-        self.assertTrue(
-            AuthenticationManager()._login(self.client, user)
-        )
+        request = RequestFactory().post('')
+        session_middleware = SessionMiddleware()
+        session_middleware.process_request(request)
+        user = authenticate(email='testuser@email.com', password='_Xxxxxxx' )
+        self.auth_manager._login(request, user)
+        self.assertEqual(request.session.get('_auth_user_id'), "1")
