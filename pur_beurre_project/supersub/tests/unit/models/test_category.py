32fcecd57583d8b198d6145df9d15d4ccf58e1a3
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-07-05 21:13:24 +0200

    pep8 on supersub/tests/models/test_category.py

--- pur_beurre_project/supersub/tests/unit/models/test_category.py
+++ pur_beurre_project/supersub/tests/unit/models/test_category.py
@@ -1,3 +1,6 @@
+# pylint: disable=C0116, E1101, W0212
+"""Test category module.
+"""
 from django.db import models
 from django.test import TestCase
 
@@ -5,18 +8,17 @@ from supersub.models.category import Category
 
 
 class CategoryTest(TestCase):
+    """Test category class.
     """
-    """  
     def test_category_with_attr_name(self):
         category_field = Category._meta.get_field('name')
         self.assertTrue(category_field)
-        self.assertEquals(type(category_field), type(models.CharField()))
-        self.assertEquals(category_field.max_length, 200)
-        self.assertEquals(category_field.unique, True)
-    
+        self.assertEqual(type(category_field), type(models.CharField()))
+        self.assertEqual(category_field.max_length, 200)
+        self.assertEqual(category_field.unique, True)
+
     def test_category_with_attr_url(self):
         category_field = Category._meta.get_field('url')
         self.assertTrue(category_field)
-        self.assertEquals(type(category_field), type(models.URLField()))
-        self.assertEquals(category_field.max_length, 200)
-
+        self.assertEqual(type(category_field), type(models.URLField()))
+        self.assertEqual(category_field.max_length, 200)
