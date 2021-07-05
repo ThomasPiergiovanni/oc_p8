84ced46e57b39ad631d0906b34099d5a4e06dbbd
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-07-05 21:23:13 +0200

    pep8 on supersub/tests/models/test_favorites.py

--- pur_beurre_project/supersub/tests/unit/models/test_favorites.py
+++ pur_beurre_project/supersub/tests/unit/models/test_favorites.py
@@ -1,3 +1,6 @@
+# pylint: disable=E1101, C0116, W0212
+"""Test Favorites module.
+"""
 from django.db import models
 from django.test import TestCase
 
@@ -7,18 +10,20 @@ from supersub.models.product import Product
 
 
 class FavoritesTest(TestCase):
-    """
+    """Test Favorites class.
     """
     def test_favorites_with_attr_product(self):
         favorites_field = Favorites._meta.get_field('product')
         self.assertTrue(favorites_field)
-        self.assertEquals(
+        self.assertEqual(
             type(favorites_field),
-            type(models.ForeignKey(Product, models.CASCADE)))
-    
+            type(models.ForeignKey(Product, models.CASCADE))
+        )
+
     def test_favorites_with_attr_custom_user(self):
         favorites_field = Favorites._meta.get_field('custom_user')
         self.assertTrue(favorites_field)
-        self.assertEquals(
+        self.assertEqual(
             type(favorites_field),
-            type(models.ForeignKey(CustomUser, models.CASCADE)))
+            type(models.ForeignKey(CustomUser, models.CASCADE))
+        )
