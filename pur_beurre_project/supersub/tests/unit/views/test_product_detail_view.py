d0262af55056fdf3d895ccfb8729f10de07a7632
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-07-06 06:49:46 +0200

    pep8 checsk

--- pur_beurre_project/supersub/tests/unit/views/test_product_detail_view.py
+++ pur_beurre_project/supersub/tests/unit/views/test_product_detail_view.py
@@ -1,3 +1,6 @@
+# pylint: disable=C0116, E1101
+"""Test productdetail view module.
+"""
 from django.test import TestCase
 
 from supersub.forms.navbar_search_form import NavbarSearchForm
@@ -7,28 +10,28 @@ from supersub.tests.integration.models.test_product import ProductTest
 
 
 class ProductDetailViewTest(TestCase):
-    """
+    """Test productdetail view class.
     """
     @classmethod
     def setUpTestData(cls):
         CategoryTest().emulate_category()
         ProductTest().emulate_product()
-    
+
     def setUp(self):
         self.response = self.client.get('/supersub/product_detail/1')
 
     def test_get_with_status_code_200(self):
-        self.assertEquals(self.response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
     def test_get_with_template(self):
         self.assertTemplateUsed(self.response, 'supersub/product_detail.html')
-    
+
     def test_get_with_navbar_form(self):
         self.assertIsInstance(
             self.response.context['navbar_form'],
-            NavbarSearchForm 
+            NavbarSearchForm
         )
-    
+
     def test_get_with_product(self):
         product = Product.objects.get(pk=1)
-        self.assertEquals(self.response.context['prod'], product)
+        self.assertEqual(self.response.context['prod'], product)
