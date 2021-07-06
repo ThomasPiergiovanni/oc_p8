8460b317e3a386abf37de3f27ccf22a03c76a40e
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-07-06 06:33:54 +0200

    pep8 checsk

--- pur_beurre_project/supersub/tests/unit/views/test_favorites_view.py
+++ pur_beurre_project/supersub/tests/unit/views/test_favorites_view.py
@@ -1,16 +1,19 @@
-from django.contrib import messages
+# pylint: disable=C0116
+"""Test Favorites view module.
+"""
 from django.test import TestCase
-from django.contrib.auth import login, logout
 
-from authentication.tests.integration.models.test_custom_user import CustomUserTest
-from supersub.views.favorites_view import FavoritesView
+from authentication.tests.integration.models.test_custom_user import (
+    CustomUserTest
+)
 from supersub.tests.integration.models.test_category import CategoryTest
 from supersub.tests.integration.models.test_favorites import FavoritesTest
 from supersub.tests.integration.models.test_product import ProductTest
+from supersub.views.favorites_view import FavoritesView
 
 
 class TestFavoritesView(TestCase):
-    """
+    """Test Favorites view class.
     """
     @classmethod
     def setUpTestData(cls):
@@ -21,31 +24,31 @@ class TestFavoritesView(TestCase):
 
     def test_init__with_favorites(self):
         self.assertTrue(self.favorites_view)
-    
+
     def test_init_with_attr_data_render(self):
         self.assertEqual(
             self.favorites_view.data['render'],
             'supersub/favorites.html'
         )
-    
+
     def test_init_with_attr_data_redirect(self):
         self.assertEqual(
             self.favorites_view.data['redirect'],
             'supersub:index'
         )
-    
+
     def test_get_with_response_200_with_favs_and_logged_in(self):
         FavoritesTest.emulate_favorites()
         self.client.login(email='testuser@email.com', password='_Xxxxxxx')
         response = self.client.get('/supersub/favorites/')
         self.assertEqual(response.status_code, 200)
-    
+
     def test_get_with_favorite(self):
         FavoritesTest.emulate_favorites()
         self.client.login(email='testuser@email.com', password='_Xxxxxxx')
         response = self.client.get('/supersub/favorites/')
         self.assertEqual(response.context['page_obj'][0].custom_user_id, 1)
-    
+
     def test_get_with_message_warning(self):
         self.client.login(
             email='testuser@email.com',
--- pur_beurre_project/supersub/tests/unit/views/test_index_view.py
+++ pur_beurre_project/supersub/tests/unit/views/test_index_view.py
@@ -1,3 +1,6 @@
+# pylint: disable=C0116
+"""Test index view module.
+"""
 from django.test import TestCase
 
 from supersub.forms.main_search_form import MainSearchForm
@@ -5,7 +8,7 @@ from supersub.forms.navbar_search_form import NavbarSearchForm
 
 
 class IndexViewTest(TestCase):
-    """
+    """Test index view class.
     """
     def test_get_with_status_code_200(self):
         response = self.client.get('/supersub/')
@@ -14,11 +17,11 @@ class IndexViewTest(TestCase):
     def test_get_with_template(self):
         response = self.client.get('/supersub/')
         self.assertTemplateUsed(response, 'supersub/index.html')
-    
+
     def test_index_with_main_form(self):
         response = self.client.get('/supersub/')
         self.assertIsInstance(response.context['main_form'], MainSearchForm)
-    
+
     def test_index_with_navbar_form(self):
         response = self.client.get('/supersub/')
         self.assertIsInstance(response.context['navbar_form'], NavbarSearchForm)
