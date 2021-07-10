24240bc32204c62b419977c0cab4b760171129f2
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-07-10 11:20:10 +0200

    test legal mentions view integration test completed

---
+++ pur_beurre_project/supersub/tests/integration/views/test_legal_mention_view.py
@@ -0,0 +1,25 @@
+# pylint: disable=C0116
+"""Test legal mention view module.
+"""
+from django.test import TestCase
+
+from supersub.forms.navbar_search_form import NavbarSearchForm
+
+
+class LegalMentionsViewTest(TestCase):
+    """Test legal mention view class.
+    """
+    def setUp(self):
+        self.response = self.client.get('/supersub/legal_mentions/')
+
+    def test_get_with_status_code_200(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_get_with_template(self):
+        self.assertTemplateUsed(self.response, 'supersub/legal_mentions.html')
+
+    def test_get_with_navbar_form(self):
+        self.assertIsInstance(
+            self.response.context['navbar_form'],
+            NavbarSearchForm
+        )
--- pur_beurre_project/supersub/tests/unit/views/test_legal_mentions_view.py
+++ pur_beurre_project/supersub/tests/unit/views/test_legal_mentions_view.py
@@ -1,25 +1,23 @@
 # pylint: disable=C0116
-"""Test legal mention view module.
+"""Test legal mentions view module.
 """
 from django.test import TestCase
 
-from supersub.forms.navbar_search_form import NavbarSearchForm
+from supersub.views.legal_mentions_view import LegalMentionsView
 
 
-class LegalMentionsViewTest(TestCase):
-    """Test legal mention view class.
+class TestLegalMentionsView(TestCase):
+    """Test legal mentions view class.
     """
-    def setUp(self):
-        self.response = self.client.get('/supersub/legal_mentions/')
+    @classmethod
+    def setUpTestData(cls):
+        cls.legal_mentions_view = LegalMentionsView()
 
-    def test_get_with_status_code_200(self):
-        self.assertEqual(self.response.status_code, 200)
+    def test_init__with_legal_mentions_view(self):
+        self.assertTrue(self.legal_mentions_view)
 
-    def test_get_with_template(self):
-        self.assertTemplateUsed(self.response, 'supersub/legal_mentions.html')
-
-    def test_get_with_navbar_form(self):
-        self.assertIsInstance(
-            self.response.context['navbar_form'],
-            NavbarSearchForm
+    def test_init_with_attr_data_render(self):
+        self.assertEqual(
+            self.legal_mentions_view.data['render'],
+            'supersub/legal_mentions.html'
         )
