8484440bea9376ccd7f0e9dac91e41ace4f2d778
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-07-10 10:54:47 +0200

    test index view integration test completed

--- pur_beurre_project/supersub/tests/unit/views/test_index_view.py
+++ pur_beurre_project/supersub/tests/unit/views/test_index_view.py
@@ -3,25 +3,21 @@
 """
 from django.test import TestCase
 
-from supersub.forms.main_search_form import MainSearchForm
-from supersub.forms.navbar_search_form import NavbarSearchForm
+from supersub.views.index_view import IndexView
 
 
-class IndexViewTest(TestCase):
+class TestIndexView(TestCase):
     """Test index view class.
     """
-    def test_get_with_status_code_200(self):
-        response = self.client.get('/supersub/')
-        self.assertEqual(response.status_code, 200)
+    @classmethod
+    def setUpTestData(cls):
+        cls.index_view = IndexView()
 
-    def test_get_with_template(self):
-        response = self.client.get('/supersub/')
-        self.assertTemplateUsed(response, 'supersub/index.html')
+    def test_init__with_index_view(self):
+        self.assertTrue(self.index_view)
 
-    def test_index_with_main_form(self):
-        response = self.client.get('/supersub/')
-        self.assertIsInstance(response.context['main_form'], MainSearchForm)
-
-    def test_index_with_navbar_form(self):
-        response = self.client.get('/supersub/')
-        self.assertIsInstance(response.context['navbar_form'], NavbarSearchForm)
+    def test_init_with_attr_data_render(self):
+        self.assertEqual(
+            self.index_view.data['render'],
+            'supersub/index.html'
+        )
