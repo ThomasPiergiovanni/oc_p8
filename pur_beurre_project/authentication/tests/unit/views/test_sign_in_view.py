2efa0e44168d6d668a2c24fffe507e3a98915efc
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-07-10 13:23:47 +0200

    test sign in view integration test completed

--- pur_beurre_project/authentication/tests/unit/views/test_sign_in_view.py
+++ pur_beurre_project/authentication/tests/unit/views/test_sign_in_view.py
@@ -1,60 +1,29 @@
+# pylint: disable=C0116
+"""Test sign in view module.
+"""
 from django.test import TestCase
 
-from authentication.forms.sign_in_form import SignInForm
-from authentication.tests.unit.models.test_custom_user import CustomUserTest
-from supersub.forms.navbar_search_form import NavbarSearchForm
+from authentication.views.sign_in_view import SignInView
 
 
-class SignInViewTest(TestCase):
-    """
+class TestSignInView(TestCase):
+    """Test sign in view class.
     """
     @classmethod
     def setUpTestData(cls):
-        CustomUserTest.emulate_custom_user()
+        cls.sign_in_view = SignInView()
 
-    def setUp(self):
-        self.response_get = self.client.get('/authentication/sign_in/')
+    def test_init__with_sign_in_view(self):
+        self.assertTrue(self.sign_in_view)
 
-    def test_get_with_status_code_200(self):
-        self.assertEqual(self.response_get.status_code, 200)
-    
-    def test_get_with_template(self):
-        self.assertTemplateUsed(
-            self.response_get, 'authentication/sign_in.html'
-        )
-
-    def test_get_with_form(self):
-        self.assertIsInstance(
-            self.response_get.context['form'], SignInForm
-        )
-
-    def test_get_with_navbarform(self):
-        self.assertIsInstance(
-            self.response_get.context['navbar_form'], NavbarSearchForm
-        )
-    
-    def test_post_with_redirect(self):
-        response = self.client.post(
-            '/authentication/sign_in/',
-            data={
-                'username': 'testuser@email.com',
-                'password': '_Xxxxxxx'
-            },
-            follow=True
-        )
+    def test_init_with_attr_data_render(self):
         self.assertEqual(
-            response.redirect_chain[0][0], '/supersub/'
+            self.sign_in_view.data['render'],
+            'authentication/sign_in.html'
         )
 
-    def test_post_with_render(self):
-        response = self.client.post(
-            '/authentication/sign_in/',
-            data={
-                'username': 'noexists@email.com',
-                'password': '_Yyyyyyy'
-            },
-            follow=True
-        )
-        self.assertIsInstance(
-            response.context['form'], SignInForm
+    def test_init_with_attr_data_redirect(self):
+        self.assertEqual(
+            self.sign_in_view.data['redirect'],
+            'supersub:index'
         )
