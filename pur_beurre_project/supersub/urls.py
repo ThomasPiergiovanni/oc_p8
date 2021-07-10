0642ad34eb926aa96f968b300fbe59e0629e0586
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-07-11 00:48:53 +0200

    pep8 related updates

--- pur_beurre_project/supersub/urls.py
+++ pur_beurre_project/supersub/urls.py
@@ -1,6 +1,8 @@
+"""Urls module
+"""
 from django.urls import path
 
-# from .views import ProductDetailView, FavoritesView, RegisterFavoriteView, LegalMentionsView
+
 from .views.favorites_view import FavoritesView
 from .views.index_view import IndexView
 from .views.legal_mentions_view import LegalMentionsView
@@ -12,9 +14,15 @@ app_name = 'supersub'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('product_detail/<int:id_prod>', ProductDetailView.as_view(), name='product_detail'),
+    path(
+        'product_detail/<int:id_prod>', ProductDetailView.as_view(),
+        name='product_detail'
+    ),
     path('favorites/', FavoritesView.as_view(), name='favorites'),
     path('results/', ResultView.as_view(), name='results'),
-    path('register_favorite/<int:id_prod>', RegisterFavoriteView.as_view(), name='register_favorite' ),
+    path(
+        'register_favorite/<int:id_prod>', RegisterFavoriteView.as_view(),
+        name='register_favorite'
+    ),
     path('legal_mentions/', LegalMentionsView.as_view(), name='legal_mentions')
 ]
