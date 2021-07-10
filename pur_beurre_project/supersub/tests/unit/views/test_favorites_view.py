73fe6c429195941bf3262cf0bafce83c3e69b35f
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-07-10 10:35:48 +0200

    test favorites view integration test completed

--- pur_beurre_project/supersub/tests/unit/views/test_favorites_view.py
+++ pur_beurre_project/supersub/tests/unit/views/test_favorites_view.py
@@ -3,12 +3,6 @@
 """
 from django.test import TestCase
 
-from authentication.tests.integration.models.test_custom_user import (
-    CustomUserTest
-)
-from supersub.tests.unit.models.test_category import CategoryTest
-from supersub.tests.unit.models.test_favorites import FavoritesTest
-from supersub.tests.unit.models.test_product import ProductTest
 from supersub.views.favorites_view import FavoritesView
 
 
@@ -18,9 +12,6 @@ class TestFavoritesView(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.favorites_view = FavoritesView()
-        CategoryTest.emulate_category()
-        ProductTest.emulate_product()
-        CustomUserTest.emulate_custom_user()
 
     def test_init__with_favorites(self):
         self.assertTrue(self.favorites_view)
@@ -36,39 +27,3 @@ class TestFavoritesView(TestCase):
             self.favorites_view.data['redirect'],
             'supersub:index'
         )
-
-    def test_get_with_response_200_with_favs_and_logged_in(self):
-        FavoritesTest.emulate_favorites()
-        self.client.login(email='testuser@email.com', password='_Xxxxxxx')
-        response = self.client.get('/supersub/favorites/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_get_with_favorite(self):
-        FavoritesTest.emulate_favorites()
-        self.client.login(email='testuser@email.com', password='_Xxxxxxx')
-        response = self.client.get('/supersub/favorites/')
-        self.assertEqual(response.context['page_obj'][0].custom_user_id, 1)
-
-    def test_get_with_message_warning(self):
-        self.client.login(
-            email='testuser@email.com',
-            password='_Xxxxxxx'
-        )
-        response = self.client.get('/supersub/favorites/', follow=True)
-        messages = response.context['messages']
-        for message in messages:
-            self.assertEqual(message.level_tag, 'warning')
-            self.assertEqual(
-                message.message,
-                "Vous n'avez enregistré aucun favoris jusqu'à présent"
-            )
-
-    def test_get_with_message_error(self):
-        response = self.client.get('/supersub/favorites/', follow=True)
-        messages = response.context['messages']
-        for message in messages:
-            self.assertEqual(message.level_tag, 'error')
-            self.assertEqual(
-                message.message,
-                "Connectez-vous pour consulter vos favoris!"
-            )
