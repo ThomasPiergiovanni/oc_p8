20c72b456670d009416891c4d85d7d68f2825e02
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-07-14 12:24:26 +0200

    pep8 related updates

--- pur_beurre_project/authentication/models.py
+++ pur_beurre_project/authentication/models.py
@@ -1,12 +1,18 @@
-from django.db import models
+"""CustomUser model module.
+"""
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from authentication.management.custom_user_manager import CustomUserManager
 
-# Create your models here.
 
 class CustomUser(AbstractUser):
+    """CustomUser model class. It sa an abstarct of django user class which
+    provides all user authenticaiton, login, etc. methods. Implemented in
+    order to have email as identifier instead of username provided by default
+    by django framework.
+    """
     username = None
     email = models.EmailField(_('email address'), max_length=250, unique=True)
     first_name = models.TextField(_('first name'), max_length=100, null=False)
