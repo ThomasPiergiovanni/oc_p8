4a4b3b2b53ef693bda24b426f449c71364cc1324
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-07-17 15:11:35 +0200

    updating settings based on heroku doc in order to have collectstatic works properly

--- pur_beurre_project/pur_beurre/settings.py
+++ pur_beurre_project/pur_beurre/settings.py
@@ -17,7 +17,12 @@ from pur_beurre.custom_settings.environnement_variables import (
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
+# The following line is for dev
+#BASE_DIR = Path(__file__).resolve().parent.parent
+
+# The following line is for prod
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -128,7 +133,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# The following line is for prod
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
+# The following line is for prod
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 AUTH_USER_MODEL = "authentication.CustomUser"
