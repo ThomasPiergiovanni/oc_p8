06bd3b62a4356a7a1290e6c3fc24d857a3a64948
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-07-24 15:40:55 +0200

    updates due to W3C checks.

--- authentication/forms/sign_up_form.py
+++ authentication/forms/sign_up_form.py
@@ -23,7 +23,6 @@ class SignUpForm(UserCreationForm):
         widget=forms.EmailInput(
             attrs={
                 'class': 'form-control',
-                'autofocus': False,
                 'id': 'id_email_input'
             }
         )
